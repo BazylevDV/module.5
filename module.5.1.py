d477d1637068f930bf34cef6e0e34ee045a6bc84
BazylevDV <dmbazylev@gmail.com>
2024-08-07 14:16:38 +0500

    Add files via upload

---
+++ module.5.1.py
@@ -0,0 +1,10 @@
+class House:
+    def __init__(self, name):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+        def go_to(1, new_floor):
+            if new_floor > self.number_of_floors or new_floor < 1:
+                print("Такого этажа не сущестует")
+                else:
+                    print(int(input(self.number_of_floors)))
